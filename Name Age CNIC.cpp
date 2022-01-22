1679d40a9af1c3e09f50b4aced28c7787a503046
adnanashiq89 <adnanashiq89@gmail.com>
2022-01-22 21:22:05 +0500

    First commit

---
+++ Name Age CNIC.cpp
@@ -0,0 +1,96 @@
+# include <iostream>
+using namespace std;
+int main ()
+	{
+	int age;
+	cout<<"enter your age";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"you can vote";
+	}
+	else
+	{
+		cout<<"you cant vote";
+	}
+		return 0;
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	/*
+		string name;
+		cout<<"Enter your name";
+		cin>>name;
+		
+		int age;
+		cout<<"Enter your age";
+		cin>>age;
+		
+		int CNIC;
+		cout<<"Enter your CNIC";
+		cin>>CNIC;
+		
+		string fruit1;
+		cout<<"Enter your friut1";
+		cin>>fruit1;
+		
+		string fruit2;
+		cout<<"Enter your fruit2";
+		cin>>fruit2;
+		
+		string fruit3;
+		cout<<"Enter your friut3";
+		cin>>fruit3;
+		
+		string vegitable1;
+		cout<<"Enter your vegitable1";
+		cin>>vegitable1;
+		
+		string vegitable2;
+		cout<<"Enter your vegitable2";
+		cin>>vegitable2;
+		
+		string vegitable3;
+		cout<<"Enter your vegitable3";
+		cin>>vegitable3;
+		
+		string sport;
+		cout<<"Enter your sport";
+		cin>>sport;
+		
+		string game;
+	
+		cout<<"Enter your game";
+		cin>>game;
+	
+		cout <<"your n name"<<name <<"your age"<<age <<"your CNIC"<<CNIC <<"your fruit1"<<fruit1 <<"your fruit2"<<fruit2 <<"your fruit3"<<fruit3 <<"your vegitable1"<<vegitable1 <<"your vegitable2"<<vegitable2 <<"your vegitable3"<<vegitable3 <<"your sport"<<sport <<"your game"<<game; 
+	*/	
+	
+	
+
+	
---
+++ Untitled12.cpp
@@ -0,0 +1,17 @@
+# include <<iostream.h>>
+
+int main ()
+	{
+		string name;
+		cout<<"Enter your name";
+		cin>>name;
+		int age;
+		cout<<"Enter your age";
+		cin>>age;
+		int CNIC;
+		cout<<"Enter your CNIC";
+		cin>>CNIC;
+		
+		cout <<"your name"<<name <<"your age"<<age <<"your CNIC"<<CNIC;
+		
+	}
